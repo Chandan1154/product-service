adb5d41269240897f6a5da903b41aadd17334c8a
Chandan C C <chandan18cse@gmail.com>
2026-02-01 22:51:28 +0530

    product service ready

--- src/main/java/com/practice/product_service/ProductServiceApplication.java
+++ src/main/java/com/practice/product_service/ProductServiceApplication.java
@@ -2,8 +2,9 @@ package com.practice.product_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jackson.autoconfigure.JacksonAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = JacksonAutoConfiguration.class)
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/practice/product_service/ProductServiceApplicationTests.java
+++ src/test/java/com/practice/product_service/ProductServiceApplicationTests.java
@@ -1,13 +1,95 @@
 package com.practice.product_service;
 
+import com.practice.product_service.dto.ProductRequest;
+import com.practice.product_service.dto.ProductResponse;
+import com.practice.product_service.model.Product;
+import com.practice.product_service.repository.ProductRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.mongodb.MongoDBContainer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
 class ProductServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.2");
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @BeforeEach
+    void setup() {
+        productRepository.deleteAll();
+    }
+
+    @TestConfiguration
+    static class TestConfig {
+        @Bean
+        ObjectMapper objectMapper() {
+            return new ObjectMapper();
+        }
+    }
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
+
+    @Test
+    void createProduct() throws Exception {
+        ProductRequest productRequest = getProductRequest();
+        String s = objectMapper.writeValueAsString(productRequest);
+        mockMvc.perform(post("/api/products").contentType(MediaType.APPLICATION_JSON).content(s)).andExpect(status().isCreated());
+        Assertions.assertEquals(1, productRepository.findAll().size());
+    }
+
+    private ProductRequest getProductRequest() {
+        return ProductRequest.builder().name("Apple").description("Iphone").price(BigDecimal.valueOf(12344)).build();
+    }
+
+    @Test
+    void getAllProducts_shouldReturnAllProducts() {
+        productRepository.deleteAll();
+        ProductRequest productRequest = getProductRequest();
+        Product product = Product.builder()
+                .name("Apple")
+                .description("Iphone")
+                .price(BigDecimal.valueOf(12344))
+                .build();
+        productRepository.save(product);
+
+        List<ProductResponse> products = productRepository.findAll()
+                .stream()
+                .map(p -> new ProductResponse(p.getId(), p.getName(), p.getDescription(), p.getPrice()))
+                .toList();
+        Assertions.assertEquals(1, products.size());
+
+    }
 }
